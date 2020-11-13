c7be3afee9f64cca144de74ff9a19a0671020089
gleb manov <glebmanov@gmail.com>
2020-11-13 17:37:11 +0300

    fifth step of project

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import { getName } from '../src/cli.js';
+import { isEvenGame } from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 
-getName();
+isEvenGame();
