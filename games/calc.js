2ebfab6dc90fccb501b7d5c6177896b6285dba6e
gleb manov <glebmanov@gmail.com>
2020-11-18 13:36:09 +0300

    update calc game

--- games/calc.js
+++ games/calc.js
@@ -3,12 +3,27 @@ import { getRandomInt, getRandomIndx } from '../src/cli.js';
 import { checkAnswer } from '../src/index.js';
 
 const operators = ['+', '-', '*'];
+const expression = (num1, num2, randomOperator) => {
+  let result;
+  switch (randomOperator) {
+    case '+': result = num1 + num2;
+      break;
+    case '-': result = num1 - num2;
+      break;
+    case '*': result = num1 * num2;
+      break;
+    // no default
+  }
+  return result;
+};
 
 export default async () => {
   const randomOperator = operators[getRandomIndx()];
-  const currentRound = `${getRandomInt()} ${randomOperator} ${getRandomInt()}`;
+  const num1 = getRandomInt();
+  const num2 = getRandomInt();
+  const currentRound = `${num1} ${randomOperator} ${num2}`;
   console.log(`Question: ${currentRound}`);
   const userAnswer = Number(await promptly.prompt('Your answer: '));
-  const correctAnswer = eval(currentRound);
+  const correctAnswer = expression(num1, num2, randomOperator);
   return checkAnswer(userAnswer, correctAnswer);
 };
