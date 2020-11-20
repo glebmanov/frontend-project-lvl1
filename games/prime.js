6d06e74f49f0fd05206671fc740dad45e01b4db0
gleb manov <glebmanov@gmail.com>
2020-11-20 12:43:20 +0300

    fix function prime.js

--- games/prime.js
+++ games/prime.js
@@ -1,22 +1,10 @@
-import getRandomInt from '../src/utils.js';
+import getRandomInt, { isPrime } from '../src/utils.js';
 import { round } from '../src/index.js';
 
 export const task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-const isPrime = (num) => {
-  if (num < 2) {
-    return 'no';
-  }
-  for (let divisor = 2; divisor <= num / 2; divisor += 1) {
-    if (num % divisor === 0) {
-      return 'no';
-    }
-  }
-  return 'yes';
-};
-
 export default async () => {
   const currentRound = getRandomInt(1, 99);
-  const correctAnswer = isPrime(currentRound);
+  const correctAnswer = isPrime(currentRound) ? 'yes' : 'no';
   return round(currentRound, correctAnswer);
 };
--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,15 @@
 export default (first, last) => Math.floor(Math.random() * (last - first + 1)) + first;
+
 export const isEven = (integer) => integer % 2 === 0;
+
+export const isPrime = (num) => {
+  if (num < 2) {
+    return false;
+  }
+  for (let divisor = 2; divisor <= num / 2; divisor += 1) {
+    if (num % divisor === 0) {
+      return false;
+    }
+  }
+  return true;
+};
