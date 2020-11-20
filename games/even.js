b22dd58a595e25558caa83eb262a6688868abc98
gleb manov <glebmanov@gmail.com>
2020-11-20 12:26:23 +0300

    fix function even.js

--- games/even.js
+++ games/even.js
@@ -1,10 +1,10 @@
-import getRandomInt from '../src/utils.js';
+import getRandomInt, { isEven } from '../src/utils.js';
 import { round } from '../src/index.js';
 
 export const task = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 export const even = async () => {
   const currentRound = getRandomInt(1, 99);
-  const correctAnswer = currentRound % 2 === 0 ? 'yes' : 'no';
+  const correctAnswer = isEven(currentRound) ? 'yes' : 'no';
   return round(currentRound, correctAnswer);
 };
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,2 @@
 export default (first, last) => Math.floor(Math.random() * (last - first + 1)) + first;
+export const isEven = (integer) => integer % 2 === 0;
